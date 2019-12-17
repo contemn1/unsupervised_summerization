5ebba181b47a2fd93622af345eb71c929cb8b322
contemn1 <xunjiez614218@gmail.com>
2019-12-17 10:36:30 -0800

    add methods to create test dataset

--- io_util.py
+++ io_util.py
@@ -4,13 +4,14 @@ import logging
 import os
 import sys
 from collections import deque
+from shutil import copyfile
 
 
 def output_iterator(file_path, output_list, process=lambda x: x):
     try:
         with io.open(file_path, mode="w+", encoding="utf-8") as file:
             for line in output_list:
-                file.write(process(line) + "\n")
+                file.write(process(line) + "/n")
     except IOError as error:
         logging.error("Failed to open file {0}".format(error))
         sys.exit(1)
@@ -27,11 +28,33 @@ def read_file(file_path, encoding="utf-8", preprocess=lambda x: x.strip()):
         sys.exit(1)
 
 
-def has_hex(s):
+def has_hex(s: str):
     """Returns a heximal formated SHA1 hash of the input string."""
     h = hashlib.sha1()
     h.update(s)
     return h.hexdigest()
 
-if __name__ == '__main__':
-    root_dir = "/home/zxj/Downloads/dailymail/stories_test"
+def get_url_hashes(url_list):
+  return [hashhex(url) for url in url_list]
+
+def hashhex(s):
+  """Returns a heximal formated SHA1 hash of the input string."""
+  if isinstance(s, str) and (sys.version_info > (3, 0)):
+      s = s.encode("utf-8")
+  h = hashlib.sha1()
+  h.update(s)
+  return h.hexdigest()
+
+def generate_test_set(file_path, cnn_dir, daily_mail_dir, output_dir):
+    file_list = read_file(file_path)
+    url_hashes = get_url_hashes(file_list)
+    story_fnames = [s+".story" for s in url_hashes]
+    num_stories = len(story_fnames)
+    for name in story_fnames:
+        cnn_path = os.path.join(cnn_dir, name)
+        daily_mail_path = os.path.join(daily_mail_dir, name)
+        output_path = os.path.join(output_dir, name)
+        if os.path.isfile(cnn_path):
+            copyfile(cnn_path, output_path)
+        elif os.path.isfile(daily_mail_path):
+            copyfile(daily_mail_path, output_path)
