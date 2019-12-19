67dec33e43ff2d8791b6f346558ab6ee6b73704d
contemn1 <xunjiez614218@gmail.com>
2019-12-18 23:25:34 -0800

    detach config and generation, fix bugs in output_iterator

---
+++ config.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+def init_argument_parser():
+    parser = argparse.ArgumentParser(description="Sentence Evaluation")
+
+    parser.add_argument("--input-dir", type=str, metavar="N",
+                        default="/home/zxj/Downloads/cnn/stories_test",
+                        help="path of data directory")
+
+    parser.add_argument("--batch-size", type=int,
+                        default=32,
+                        help="path of glove file")
+
+    parser.add_argument("--use-cuda", action='store_true',
+                        default=False,
+                        help="whether to use cuda")
+
+    parser.add_argument("--half-precision", action='store_true',
+                        default=False,
+                        help="whether to use half precision inference")
+
+    parser.add_argument("--output-dir", type=str,
+                        default="/home/zxj/Downloads/cnn/output")
+
+    parser.add_argument("--use-multiple-gpu", action='store_true',
+                        default=False, help='whether to use multiple gpus at inference stage')
+    return parser
